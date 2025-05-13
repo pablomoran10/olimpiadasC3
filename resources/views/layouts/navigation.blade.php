62d3c917750225f23c983d41f210c0d62ee940d0
Alberto Sierra <alberto.sierra@murciaeduca.es>
2025-05-13 09:32:13 +0200

    Eliminando enlace a dashboard en admin

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -11,11 +11,13 @@
                 </div>
 
                 <!-- Navigation Links -->
+                <!--
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+                -->
                 @can('viewAny', App\Models\Ciclo::class)
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('ciclos.index')" :active="request()->routeIs('ciclos.index')">
